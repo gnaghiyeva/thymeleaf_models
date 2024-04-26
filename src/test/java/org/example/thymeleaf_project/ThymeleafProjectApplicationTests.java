cc9f806b906b39f81878281865eebbfbe8718ed7
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2024-04-26 15:34:43 +0400

    create-controller

---
+++ src/test/java/org/example/thymeleaf_project/ThymeleafProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.thymeleaf_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
