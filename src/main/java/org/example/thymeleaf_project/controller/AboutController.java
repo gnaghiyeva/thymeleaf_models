3026ae35081d15f8e6a7db429079327bf05a796e
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2024-04-27 13:56:53 +0400

    about-page

---
+++ src/main/java/org/example/thymeleaf_project/controller/AboutController.java
@@ -0,0 +1,13 @@
+package org.example.thymeleaf_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutController {
+    @GetMapping("/about")
+    public String index() {
+        return "about";
+    }
+
+}
